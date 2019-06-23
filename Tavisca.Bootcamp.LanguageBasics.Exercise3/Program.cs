b69381cf158747c6b1e41d77d30cb229baca41b0
Raunak singh <51930237+tavisca-rsingh@users.noreply.github.com>
2019-06-23 15:05:25 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
-
+using System.Collections.Generic;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -41,7 +41,95 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            int[] dietResult = new int[dietPlans.Length];
+            int[] calories = new int[protein.Length];
+
+            /* Computing calories for each index */
+            for (int i = 0; i < protein.Length; i++)
+                 calories[i] = protein[i] * 5 + carbs[i] * 5 + fat[i] * 9; 
+
+               for (int i = 0; i < dietPlans.Length; i++)
+            {
+                 List<int> first = new List<int>();
+                 
+                 for(int k=0;k<protein.Length;k++)
+                     first.Add(k);
+                   
+                 string diet = dietPlans[i];
+                
+                 if(diet=="") dietResult[i]=0;
+                 else 
+                 {
+                    for(int j=0;j<diet.Length;j++)
+                    {    
+                        if(diet[j]=='P')
+                         { 
+                          first = makeNewListMAX(protein,first);
+                         }
+                       else if(diet[j]=='p')
+                         {
+                          first = makeNewListMIN(protein,first);
+                         }
+                       else if(diet[j]=='C')
+                         {
+                           first = makeNewListMAX(carbs,first);
+                         }
+                       else if(diet[j]=='c')
+                         {
+                           first = makeNewListMIN(carbs,first);
+                         }
+                       else if(diet[j]=='F')
+                         {
+                           first = makeNewListMAX(fat,first);
+                         }
+                       else if(diet[j]=='f')
+                         {
+                           first = makeNewListMIN(fat,first);
+                         }
+                       else if(diet[j]=='T')
+                         {
+                           first = makeNewListMAX(calories,first);
+                         }
+                       else if(diet[j]=='t')
+                         {
+                           first = makeNewListMIN(calories,first);
+                         } 
+                     }
+                 }
+           
+                  dietResult[i] = first[0];
+            }
+              return dietResult;
+             throw new NotImplementedException();
         }
+       
+       /* Computing list for max */
+       public static List<int> makeNewListMAX(int[] item, List<int> first)
+        {
+            int element = item[first[0]];
+            List<int> newList = new List<int>();
+            for(int i =1;i<first.Count;i++)
+                 if(element<item[first[i]])
+                     element = item[first[i]];    
+            
+            foreach(int i in first)
+                 if(item[i]==element) 
+                  newList.Add(i);
+            return newList;
+        }
+        /* Computing list for min */
+       public static List<int> makeNewListMIN(int[] item, List<int> first)
+        {
+            int element = item[first[0]];
+            List<int> newList = new List<int>();
+            for(int i =1;i<first.Count;i++)
+                 if(element>item[first[i]])
+                     element = item[first[i]];    
+            
+            foreach(int i in first)
+                 if(item[i]==element) 
+                  newList.Add(i);
+            return newList;
+        }       
     }
 }
